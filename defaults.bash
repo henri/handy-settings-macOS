8cc49bfda76ff4c0d9935be8f1c568ea83e35c0a
henri <github.shustak@neverbox.com>
2022-10-18 13:26:41 +1300

    many deafults updates

--- defaults.bash
+++ defaults.bash
@@ -1,3 +1,14 @@
-# Set Menu Bar Clock to Analog
+# set menu bar clock to analog
 defauls write com.apple.menuextra.clock IsAnalog -bool true
 
+# show expanded save dialog box by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+
+# do not warn about file name extension (suffix) changes
+defaults write com.apple.finder "FXEnableExtensionChangeWarning" -bool "false" && killall Finder
+
+# ask to confirm changes before saving files (disable autosave)
+defaults write ~/Library/Preferences/.GlobalPreferences.plist NSCloseAlwaysConfirmsChanges 1
+
+# enable keyboard navigation of controls
+defaults write .GlobalPreferences.plist AppleKeyboardUIMode 2
