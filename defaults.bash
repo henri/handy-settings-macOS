76e19cbc93557acaa0707c0c7a6273b873f68709
henri <github.shustak@neverbox.com>
2022-10-18 13:31:32 +1300

    Update defaults.bash
    added a safari option

--- defaults.bash
+++ defaults.bash
@@ -5,10 +5,13 @@ defauls write com.apple.menuextra.clock IsAnalog -bool true
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 
 # do not warn about file name extension (suffix) changes
-defaults write com.apple.finder "FXEnableExtensionChangeWarning" -bool "false" && killall Finder
+defaults write com.apple.finder "FXEnableExtensionChangeWarning" -bool "false" # && killall Finder
 
 # ask to confirm changes before saving files (disable autosave)
 defaults write ~/Library/Preferences/.GlobalPreferences.plist NSCloseAlwaysConfirmsChanges 1
 
 # enable keyboard navigation of controls
 defaults write .GlobalPreferences.plist AppleKeyboardUIMode 2
+
+@ safari show full url in smart search filed 
+defaults write com.apple.safari "ShowFullURLInSmartSearchField" -bool "true" # && killall Safari
