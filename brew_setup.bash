60d7deb3339f40ec3f8da6e78f29ddf0776c5752
henri <github.shustak@neverbox.com>
2022-11-09 09:02:11 +1300

    Update brew_setup.bash

--- brew_setup.bash
+++ brew_setup.bash
@@ -3,18 +3,20 @@
 # Brew is a package maanager for GNU/LINUX and macOS. This is a script which you may modify to meet your needs to quickly install a 
 # bunch of packages onto your system so you are ready to go. Just modify to meet your needs.
 #
-# Before using this script you should install brew. Visit brew.sh and get the install command.
-# If you are feeling lucky, you could uncomment the line below - your milage may vary.
 #
 # (C)2022 Henri Shustak
 # Released under the MIT Licence : https://mit-license.org
 #
-
+#
 # NOTE (1) : It is easiest to run this as as a user who can sudo on the system.
 # NOTE (2) : 
-
+#
+# Before using this script you should install brew. Visit brew.sh and get the install command.
+# If you are feeling lucky, you could uncomment the line below - your milage may vary.
+#
 # Uncomment the line below to install brew - you would be better off visiting www.brew.sh and making sure you get the real deal.
 # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 
 # check the brewing setup is ready for production
 brew doctor
