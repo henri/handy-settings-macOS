6e77f2215296eb33d728b521ec4038e8953ca1da
henri <github.shustak@neverbox.com>
2022-11-11 00:16:35 +1300

    Update defaults_setup.bash

--- defaults_setup.bash
+++ defaults_setup.bash
@@ -30,6 +30,12 @@ defauls write com.apple.menuextra.clock IsAnalog -bool true
 # add volume controls to the menu bar
 defaults write com.apple.systemuiserver menuExtras -array-add "/System/Library/CoreServices/Menu Extras/Volume.menu" # && killall SystemUIServer -HUP
 
+# enable audio volume adjustment feedback 
+defaults write com.apple.systemsound "com.apple.sound.uiaudio.enabled" -int 0
+
+# disable audio volume adjustment feedback 
+defaults write com.apple.systemsound "com.apple.sound.uiaudio.enabled" -int 0
+
 # show battery percentage in the menubar
 # defaults write com.apple.menuextra.battery ShowPercent -string YES # && killall SystemUIServer -HUP
 
