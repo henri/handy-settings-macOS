77d58243769b41846e8089df597fc54d3f1c7cd2
henri <github.shustak@neverbox.com>
2022-11-11 00:13:10 +1300

    Update defaults_setup.bash

--- defaults_setup.bash
+++ defaults_setup.bash
@@ -120,4 +120,5 @@ defaults write com.apple.LaunchServices "LSQuarantine" -bool false
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
 
-
+# spaces disable automatic rearrangment based on use
+# defaults write com.apple.dock mru-spaces -bool false
