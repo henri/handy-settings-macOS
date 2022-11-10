b030ee885acd28da3616a7710c3cd7d3899e37b6
henri <github.shustak@neverbox.com>
2022-11-11 00:15:32 +1300

    Update defaults_setup.bash

--- defaults_setup.bash
+++ defaults_setup.bash
@@ -125,6 +125,3 @@ defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
 # enable tap to click on trackpad
 # defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
-# sudo defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-# sudo defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-sudo defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
